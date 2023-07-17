62b96f98236675254d3f5647c34123a0f1cbdd6a
“kel2203” <raquelsb2001@gmail.com>
2023-07-17 17:42:20 -0300

    insert tests Junit

--- api/src/main/java/med/voll/api/domain/consultas/Consulta.java
+++ api/src/main/java/med/voll/api/domain/consultas/Consulta.java
@@ -34,6 +34,7 @@ public class Consulta {
     @Enumerated(EnumType.STRING)
     private MotivoCancelamento motivoCancelamento;
 
+
     public void cancelar(MotivoCancelamento motivo) {
         this.motivoCancelamento = motivo;
     }
